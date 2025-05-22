01adfb1ce27e286ddfcda7798504a645fc826fac
Swati Sharma <swati.sharma.7819@gmail.com>
2025-05-22 16:49:43 +0530

    Add files via upload

---
+++ Day_1.py
@@ -0,0 +1,5 @@
+#Day 1 contains introduction to programming and writing hello world in python
+print("Hello World")
+print('''Programming, also known as coding, is the process of creating a set of instructions, called a program, 
+that a computer can follow to perform a specific task or solve a particular problem. It involves writing code in a 
+programming language, which is a formal system for communicating instructions to a computer.''')
